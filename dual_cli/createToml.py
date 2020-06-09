d4909cbcfea1941a4d231b3cbb8c0049e7e4cebf
bgallois <benjamin@gallois.cc>
2020-06-09 08:30:08 +0200

    [feat] cli: add automatic order detection

--- dual_cli/createToml.py
+++ dual_cli/createToml.py
@@ -27,10 +27,18 @@ def createToml(path="", fileName=None, dest ="", erase=None):
     timestamps = pandas.read_csv(path + "Timestamps.txt", sep="\t", header=None)
     trackingData = pandas.read_csv(path + "Tracking_Result/tracking.txt", sep="\t")
     distanceData = pandas.read_csv(path + "distance.txt", sep="\t")
+    order = "XXXX"
+    with open(path + "Protocol.txt", 'r') as proto:
+        proto.readline().strip() 
+        line = proto.readline().strip() 
+        if line == "# PROTOCOL: BUFFER - LEFT - BUFFER - RIGHT":
+            order = "BLBR"
+        elif line == "# PROTOCOL: BUFFER - RIGHT - BUFFER - LEFT":
+            order = "BRBL"
 
     info = { "title" : fileName, "path" : path, "author" : "Benjamin Gallois" }
     fish = { "age" : int(parameters[1][1]), "date" : datetime.datetime.strptime(parameters[1][0], "%Y-%m-%d"),  "type" : "wt", "remark" : ""}
-    experiment = {"date" : datetime.datetime.strptime(folders[-2], "%Y-%m-%d"), "product" : parameters[1][2].lower(), "concentration" : float(parameters[1][3]), "interface" : "500", "order" : "BRBL", "buffer1" : [int(milestones[0][1]), int(milestones[0][2])], "buffer2" : [int(milestones[0][5]), int(milestones[0][6])], "product1" : [int(milestones[0][3]), int(milestones[0][4])], "product2" : [int(milestones[0][7]), int(timestamps[0].tail(1))]}
+    experiment = {"date" : datetime.datetime.strptime(folders[-2], "%Y-%m-%d"), "product" : parameters[1][2].lower(), "concentration" : float(parameters[1][3]), "interface" : "500", "order" : order, "buffer1" : [int(milestones[0][1]), int(milestones[0][2])], "buffer2" : [int(milestones[0][5]), int(milestones[0][6])], "product1" : [int(milestones[0][3]), int(milestones[0][4])], "product2" : [int(milestones[0][7]), int(timestamps[0].tail(1))]}
     metadata = { "image" : timestamps[0], "time" : timestamps[1] }
 
     tmp = np.zeros(len(trackingData.imageNumber.values))
--- dual_cli/tests/tests.py
+++ dual_cli/tests/tests.py
@@ -13,8 +13,17 @@ def test_add_distance():
     process = subprocess.call(["../venv/bin/python3", "createToml.py", "tests/2020-02-20/Test_Run/", "-o", "tests/"])
     f = toml.load("tests/20200220Test_Run.toml")
 
-    for l in [0]: # range(np.max(tracking.id.values + 1)):
+    for l in range(np.max(tracking.id.values + 1)):
         for k, (i, j) in enumerate(zip(f["tracking"]["Fish_" + str(l)]["imageNumber"], f["tracking"]["Fish_" + str(l)]["distance"])):
             if not np.isnan(j):
                 assert np.around((j), 3) == np.around(distance[(distance.imageNumber == i) & (distance.id == l)].distance.values[0], 3)
             assert np.around(f["tracking"]["Fish_" + str(l)]["xHead"][k], 3) == np.around(tracking[(tracking.imageNumber == i) & (tracking.id == l)].xHead.values[0], 3)
+
+
+def test_info():
+    f = toml.load("tests/20200220Test_Run.toml")
+
+    assert f["experiment"]["product"] == "citricacide"
+    assert f["experiment"]["concentration"] == 0.02
+    assert f["experiment"]["order"] == "BLBR"
+    assert f["fish"]["age"] == 7
